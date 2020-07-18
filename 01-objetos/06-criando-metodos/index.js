bd31f4dcf9195609ea57c17c2a74a15b15d7c2d4
yago_dev <yagomilano92@gmail.com>
2020-07-18 20:31:37 -0300

    criando metodos

---
+++ 01-objetos/06-criando-metodos/index.js
@@ -0,0 +1,8 @@
+// os metodos sao as acoes de um objeto
+// podemos utilizar metodos para resgatar propriedades do objeto ou modificar o valor delas, por exemplo
+let pessoa = {
+  nome: 'Yago',
+  falar: () => console.log(`Oi, eu sou o Yago`)
+}
+console.log(pessoa)
+pessoa.falar()
