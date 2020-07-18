b4c620312e43759f97973fcb44592d1f7f05f89e
yago_dev <yagomilano92@gmail.com>
2020-07-18 20:01:15 -0300

    criando objetos

---
+++ 01-objetos/01-criando-objetos/index.js
@@ -0,0 +1,14 @@
+// objeto: tipo de dado que possui duas caracteristicas:
+// -> propriedades("variaveis"): caracteristicas do objeto;
+// -> metodos("funcoes"): acoes/comportamento do objeto;
+let carro = {
+  portas: 4,
+  cor: 'Vermelho'
+}
+console.log(carro)
+console.log(typeof carro)
+
+let pessoa = {
+  nome: 'Yago'
+}
+console.log(pessoa)
