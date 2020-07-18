c75111f13b9670a953b6989aff54b66b5d201c36
yago_dev <yagomilano92@gmail.com>
2020-07-18 20:06:34 -0300

    propriedades do objeto

---
+++ 01-objetos/02-propriedades-do-objeto/index.js
@@ -0,0 +1,10 @@
+// propriedades sao as caracteristicas dos objetos
+// por ex: cor, portas, nome, marca...
+
+const objeto = {
+  propriedade: 'valor'
+}
+
+// acessando os valores das propriedades
+console.log(objeto.propriedade)
+console.log(objeto['propriedade'])
